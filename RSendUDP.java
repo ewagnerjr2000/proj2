b69755e403a49e217908df8ce184059f135b74c8
Ed Wagner <specialed08@gmail.com>
2014-02-02 13:54:37 -0500

    playing with timer

--- RSendUDP.java
+++ RSendUDP.java
@@ -35,8 +35,7 @@ public class RSendUDP implements RSendUDPI{
 	private String filename;
 	private UDPSocket socket;
 	private sender sender;
-	private Timer timer = new Timer();;
-//	private MyTask t;
+	private Timer timer ;//	private MyTask t;
 	private ackreceiver ackreceiver;
 	private int MTU;
 	
@@ -113,7 +112,7 @@ public class RSendUDP implements RSendUDPI{
 			e.printStackTrace();
 		}
 		//timer = new Timer("Printer");
-		acked = false;
+	//	acked = false;
         //2- Taking an instance of class contains your repeated method.
     //    t = new MyTask(array);
 		//sender = new sender(sendpacket);
@@ -144,10 +143,8 @@ public class RSendUDP implements RSendUDPI{
 			
 			while (i != totalSize)
 			{
+				acked = false;
 				header[0] = (byte)(counter);
-			//	sendpacket = new byte[header.length + filebuffer.remaining()];
-				//sendpacket = new byte[header.length + MTU-4];
-				//System.arraycopy(header, 0, sendpacket, 0, 4);
 				System.out.println("Filebuffer remaining: "+ filebuffer.remaining());
 				if (filebuffer.remaining() < (MTU - 4)){ //filebuffer is less than MTU-4
 					sendpacket = new byte[header.length + filebuffer.remaining()];
@@ -169,14 +166,16 @@ public class RSendUDP implements RSendUDPI{
 				System.out.println("Sending packet number: " + counter);
 				Thread senderpacket1= new Thread(sender);
 				Thread ackreceiver1 = new Thread(ackreceiver);
-				//Thread timer1 = new Thread(timer);
+				
 					try {
 						senderpacket1.start();
 						senderpacket1.join();
+						timer = new Timer();
 						timer.schedule(new MyTask(sendpacket),2000,2000);
+						
 						ackreceiver1.start();
 						ackreceiver1.join();
-						
+						timer.cancel();
 					}catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -184,147 +183,7 @@ public class RSendUDP implements RSendUDPI{
 					
 					counter++;
 			}
-			/*if (totalSize <= MTU)
-			{
-				System.out.println("Little file");
-				acked = false;
-			//	System.out.println("Final packet size: " + finalPacket.capacity());
-				header[0] = (byte)(counter);
-				header[3] = (byte) 0xFF; 
-				sendpacket = new byte[header.length + filebuffer.remaining()];
-				System.arraycopy(header, 0, sendpacket, 0, 4);
-				filebuffer.get(sendpacket,4,filebuffer.remaining());
-				System.out.println(sendpacket.length);
-				sender = new sender(sendpacket);
-				timer = new timer(sendpacket);
-				System.out.println("Sending packet number: " + counter);
-				Thread senderpacket1= new Thread(sender);
-				Thread ackreceiver1 = new Thread(ackreceiver);
-				Thread timer1 = new Thread(timer);
-				//ackreceiver ackreceiver1 = new ackreceiver();
-				
-				
-					
-					
-					try {
-						senderpacket1.start();
-						senderpacket1.join();
-						timer1.start();
-						ackreceiver1.start();
-						
-						ackreceiver1.join();
-						timer1.join();
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-					
-				
-				
-				
-			}
-			//if (array.length > windowSize)
-			if(totalSize > MTU)
-			{
-				 System.out.println("Big file");
-				 i = 0;
-				// length = array.length;
-				 counter = 1; 
-				while(i != filebuffer.capacity())			
-				{	
-					acked = false;
-					System.out.println("Total Size:  " + totalSize);
-					if (MTU < totalSize)
-					{
-						sendpacket = new byte[header.length + (MTU-4)];
-						//header updated to show the count
-						header[0] = (byte)counter;
-						System.out.println("sendpacket length: " + sendpacket.length);
-						System.arraycopy(header, 0, sendpacket, 0, 4);
-						filebuffer.get(sendpacket, 4,MTU-4);
-						System.out.println("file position: " + filebuffer.position());
-						totalSize = totalSize - (MTU - 4);
-						System.out.println(" Message " + counter + " with " + MTU + " bytes of actual data");
-						System.out.println("Sending packet number: " + counter);
-						sender = new sender(sendpacket);
-						Thread senderpacket2 = new Thread(sender);
-						Thread ackreceiver2 = new Thread(ackreceiver);
-						Thread timer2 = new Thread(timer);
-						try {
-							System.out.println("Acked: "+ acked);
-							senderpacket2.start();
-							//senderpacket.sleep(500);
-								
-							senderpacket2.join();
-							timer2.start();
-							ackreceiver2.start();
-							
-							ackreceiver2.join();
-							timer2.join();
-							//timer.schedule(new MyTask(sendpacket),2000,2000);
-							System.out.println("Acked: "+ acked);
-						} catch (InterruptedException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-											
-						counter++;
-						i = i + (MTU - 4);
-						
-					}
-					
-					else if (MTU > totalSize)
-					{
-						//Last packet
-						header[3] = (byte) 0xFF; 
-						//System.out.println("Position: "+ filebuffer.position());
-						System.out.println("Test2");
-						System.out.println("TotalSize: " + totalSize);
-						//header updated to show the count
-						sendpacket = new byte [header.length + totalSize]; 
-						//sendpacket = new byte [header.length + filebuffer.remaining()]; 
-						
-						System.out.println("filebuffer remaining "+ filebuffer.remaining());
-						System.arraycopy(header, 0, sendpacket, 0, 4);
-						//filebuffer.get(sendpacket,4,filebuffer.remaining());
-						filebuffer.get(sendpacket,4,totalSize);
-						//System.arraycopy(array, i, buffer1, 0, totalSize);
-						System.out.println(" Message " + counter + " with " + totalSize + " bytes of actual data");
-					//	finalPacket = ByteBuffer.allocate(header.length + buffer1.length);
-						sender = new sender(sendpacket);
-						System.out.println("Sending packet number: " + counter);
-						Thread senderpacket3 = new Thread(sender);
-						Thread ackreceiver3 = new Thread(ackreceiver);
-						Thread timer3 = new Thread(timer);
-						
-						try {
-							senderpacket3.start();
-							
-							senderpacket3.join();
-							timer3.start();
-							ackreceiver3.start();
-					//		timer.schedule(new MyTask(sendpacket), 0, 2000);
-							
-							ackreceiver3.join();
-							timer3.join();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						
-						}
-						counter++;
-						
-										
-						i = i + totalSize;
-						
-					}
-					System.out.println("i : " +  i + " total size: " + totalSize);
-						
-				}
-						
-		}
-		*/
+			
 		System.out.println(packetmap);
 		long finish_time = System.currentTimeMillis();
 		double totaltime = ((double)finish_time - (double)startTime)/1000;
@@ -470,12 +329,7 @@ public class RSendUDP implements RSendUDPI{
 	{
 		try {
 
-			/*byte [] buffer = ("Hello World").getBytes();
-			UDPSocket socket = new UDPSocket();
-		//	socket.send(new DatagramPacket(buffer, buffer.length,
- 		//		InetAddress.getByName(SERVER), PORT));
-		*/
-			// TODO Auto-generated method stub
+			
 			RSendUDP sender = new RSendUDP();
 			sender.setMode(0);
 			sender.setLocalPort(12988);
@@ -501,11 +355,11 @@ class sender implements Runnable {
 	public void run(){
 		try{
 			//if( socket.isBound()){
-			Thread.sleep(0);
+			
 				socket.send(new DatagramPacket(packet,packet.length,receiver));
 						
 		//	}
-		}catch(IOException | InterruptedException e){
+		} catch (IOException e) {
 			System.out.println("IOException, in sender ");
 		}
 	
@@ -527,22 +381,17 @@ class ackreceiver implements Runnable {
 			System.out.println("Inside Ackreceiver");
 		//	System.out.println(ackpacket.length);
 			
-					byte[] test;
 					System.out.println("Ack Receiver port: " + socket.getLocalPort());
 					try {
-						Thread.sleep(0);
-						//socket = new UDPSocket();
 						byte[] ackbuffer = new byte[4];
 						DatagramPacket ackpacket = new DatagramPacket(ackbuffer,ackbuffer.length);
-						//socket.receive(new DatagramPacket(ackpacket,ackpacket.length,receiver));
 						socket.receive(ackpacket);
-						//String s = new String(ackpacket, "UTF-8");
 						acked = true;
 						String s = new String(ackpacket.getData());
 						System.out.println("Are we acking the current packet: " + (int)s.charAt(0)+ ", " + counter);
 						System.out.println("Ackpacket: "+ (int)s.charAt(0));
 						packetmap.put((int)s.charAt(0), (int)s.charAt(1));
-					} catch (IOException | InterruptedException e) {
+					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
@@ -555,62 +404,22 @@ class ackreceiver implements Runnable {
 			
 	}
 
-class timer implements Runnable {
-	private byte[] packet;
-	public timer( byte[] packet){
-		this.packet = packet;
-	}
-	
-	
-	public void schedule(MyTask myTask, int i, int j) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	public void run() {
-		
-			System.out.println("Test ");
-			try {
-				Thread.sleep(2000);
-				if (acked == false)
-				{
-					
-					socket.send(new DatagramPacket(packet,packet.length,receiver));
-				}
-				
-				else
-				{
-					//Thread.currentThread().join();
-					System.out.println("Value has been acked");
-				}
-			} catch (InterruptedException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-			System.out.println("Timer done sleeping");
-		
-		
-	}
-	
-}
-
-
 
 class MyTask extends TimerTask {
     //times member represent calling times.
+	//private int currentcount = 0;
     private int times = 0;
     private byte[] packet;
 	public MyTask(byte[] sendpacket) {
 		this.packet = sendpacket;
+		//this.currentcount = counter;
 	}
 
 	
  
     public void run() {
         
-      //  if (acked == false)
+     
         if (acked == false) {
             System.out.println("I'm alive...");
            
@@ -624,7 +433,7 @@ class MyTask extends TimerTask {
 				e.printStackTrace();
 			}
 		
-           
+      //    System.out.println("Hash map: " + packetmap.get(currentcount));
         } else {
             System.out.println("Timer stops now...");
  
